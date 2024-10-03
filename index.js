6e1fd9f1c2bd902caa2759a005d311ea1a2c2a55
ICE-awa <HaydenIce@outlook.com>
2024-10-03 21:39:21 +0800

    update Oct.3

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log("index.js enabled");
 
-    let max_width = window.screen.width;
-    let max_height = window.screen.height;
+    // let max_width = window.screen.width;
+    // let max_height = window.screen.height;
 
     // let html = document.getElementsByTagName("html");
     // let body = document.getElementsByTagName("body");
@@ -11,8 +11,8 @@ document.addEventListener('DOMContentLoaded', () => {
     // body.style.width = max_width;
     // body.style.height = max_height;
 
-    document.documentElement.style.setProperty('--screen-width', max_width + 'px');
-    document.documentElement.style.setProperty('--screen-height', max_height + 'px');
+    // document.documentElement.style.setProperty('--screen-width', max_width + 'px');
+    // document.documentElement.style.setProperty('--screen-height', max_height + 'px');
 
     // Header
 
@@ -23,16 +23,48 @@ document.addEventListener('DOMContentLoaded', () => {
        if(!logo_pic_click){
             // console.log("click");
             logo_pic_click = true;
-            logo_pic.style.transition = "all .1s";
+            logo_pic.style.transition = "all .3s";
             logo_pic.style.transform = "scale(0.9)";
+            sidebar_left.style.animation = "sidebar_left_display 0.3s";
+            sidebar_left.style.display = "flex";
             setTimeout(function(){
-                sidebar_left.style.animation = "sidebar_left_display 0.2s";
-                sidebar_left.style.display = "flex";
                 sidebar_left.style.transform = "translateX(0%)";
                 logo_pic_click = false;
-                logo_pic.style.transition = "all .2s";
+                logo_pic.style.transition = "all .3s";
                 logo_pic.style.transform = "scale(1.0)";
-            }, 100)
+            }, 300)
+        }
+    });
+
+    let nav_btn_1 = document.getElementById("nav_btn_1");
+    let nav_btn_2 = document.getElementById("nav_btn_2");
+    let nav_btn_3 = document.getElementById("nav_btn_3");
+    let nav_btn_click = false;
+    nav_btn_1.addEventListener("click", function (evt) {
+        if(!nav_btn_click){
+            nav_btn_click = true;
+            console.log(this.innerHTML + 'btn is clicked');
+            setTimeout(function(){
+                nav_btn_click = false;
+            }, 300)
+        }
+    });
+    nav_btn_2.addEventListener("click", function (evt) {
+        if(!nav_btn_click){
+            nav_btn_click = true;
+            console.log(this.innerHTML + 'btn is clicked');
+            setTimeout(function(){
+                nav_btn_click = false;
+            }, 300)
+        }
+    });
+    nav_btn_3.addEventListener("click", function (evt) {
+        if(!nav_btn_click){
+            nav_btn_click = true;
+            console.log(this.innerHTML + 'btn is clicked');
+            setTimeout(function(){
+                nav_btn_click = false;
+            }, 300)
         }
     });
 
@@ -44,16 +76,16 @@ document.addEventListener('DOMContentLoaded', () => {
         if(!sidebar_logo_pic_click){
             // console.log("click");
             sidebar_logo_pic_click = true;
-            sidebar_logo_pic.style.transition = "all .1s";
+            sidebar_logo_pic.style.transition = "all .3s";
             sidebar_logo_pic.style.transform = "scale(0.9)";
+            sidebar_left.style.animation = "sidebar_left_hidden 0.3s";
             setTimeout(function(){
-                sidebar_left.style.animation = "sidebar_left_hidden 0.2s";
                 sidebar_left.style.display = "none";
                 sidebar_left.style.transform = "translateX(-100%)";
                 sidebar_logo_pic_click = false;
-                sidebar_logo_pic.style.transition = "all .2s";
+                sidebar_logo_pic.style.transition = "all .3s";
                 sidebar_logo_pic.style.transform = "scale(1.0)";
-            }, 100)
+            }, 300)
         }
     });
 })
